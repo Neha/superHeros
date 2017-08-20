81b7a294494afa32f1b2d05d2f12796a68aa823a
nehasharm <nehasharm@nehasharm-macbookpro.roam.corp.google.com>
2017-08-20 11:56:18 +0530

    Added images folder and added the every() function code.

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 var superHeroData = [
-  { heroName: "superman", power: "fly", score: 10 },
-  { heroName: "Batman", power: "rich", score: 0 },
-  { heroName: "Ironman", power: "rich", score: 0 },
-  { heroName: "wonderwoman", power: "fly", score: 10 },
-  { heroName: "Hulk", power: "anger", score: 5 },
-  { heroName: "Ant", power: "size", score: 20 },
-  { heroName: "Flash", power: "fast", score: 15 },
-  { heroName: "captain America", power: "rich", score: 0 }
+  { heroName: "superman", power: "fly", score: 10, superHero : 'yes'  },
+  { heroName: "Batman", power: "rich", score: 0 , superHero : 'yes'},
+  { heroName: "Ironman", power: "rich", score: 0, superHero : 'yes' },
+  { heroName: "wonderwoman", power: "fly", score: 10, superHero : 'yes' },
+  { heroName: "Hulk", power: "anger", score: 5, superHero : 'yes' },
+  { heroName: "Ant", power: "size", score: 20, superHero : 'yes' },
+  { heroName: "Flash", power: "fast", score: 15, superHero : 'yes' },
+  { heroName: "captain America", power: "rich", score: 0, superHero : 'yes' }
 ];
 
 var heroNameList = [],
@@ -14,20 +14,28 @@ var heroNameList = [],
     heroScoreList = [],
     scorePlayer;
 
-const filter = document.querySelectorAll("span.filter"),
+const filterBtn = document.querySelectorAll("span.filter"),
       data = document.getElementById("data"),
       score = document.getElementById("score");
 
-for (let i = 0; i < filter.length; i++) {
-   filter[i].addEventListener("click", getData)
+//every
+var isSuperHero = superHeroData.every(function(elm){
+        return elm.superHero == 'yes';
+        });
+     
+for (let i = 0; i < filterBtn.length; i++) {
+   filterBtn[i].addEventListener("click", getData)
 }
 
 function getData(){
-    heroScoreList = [];
-    data.innerHTML = "";
-    score.innerHTML = "";
+    let _this = this;
+    
+    if(isSuperHero){
+           heroScoreList = [];
+           data.innerHTML = "";
+           score.innerHTML = "";
 
-    superPower = this.getAttribute("data-attr");
+    superPower = _this.getAttribute("data-attr");
     
     //filter
     let flyhero = superHeroData.filter((elm) => {
@@ -52,5 +60,22 @@ function getData(){
     let subHero = heroNameList.some((elm) => {
       return elm === "Batman";
     });
-  
+     
+      } 
+  else{
+    data.innerHTML = 'Superheros are out to save the world!!'
+  }
+
+
+   
 }
+    
+  
+
+
+
+
+
+    
+
+
